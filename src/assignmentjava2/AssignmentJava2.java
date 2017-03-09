93ac8c562b4719c9876ee24f8ecbea8c1b63bf32
Thang <anhthang3011@gmail.com>
2017-03-09 10:51:58 +0700

    Update

--- src/assignmentjava2/AssignmentJava2.java
+++ src/assignmentjava2/AssignmentJava2.java
@@ -5,19 +5,12 @@
  */
 package assignmentjava2;
 
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.InputStreamReader;
-import java.io.ObjectOutputStream;
 import java.io.OutputStreamWriter;
-import java.nio.charset.Charset;
-import java.util.ArrayList;
 import java.util.HashMap;
 
 /**
